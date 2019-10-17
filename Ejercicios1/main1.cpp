d4e9cda54685d39cebcfbc2b5ecffd2264663b57
Tom-96 <pflores@DESKTOP-C9Q6AMG.localdomain>
2019-10-17 13:30:58 -0300

    ejercicio 1

---
+++ Ejercicios1/main1.cpp
@@ -0,0 +1,68 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+#include <stdlib.h>
+
+float* getTransformTable(int a, int b)
+{
+    float *preCalculatedTable = new float[256];
+
+    for (int i=0;i<256;i++)
+    {
+        if (i <= a)
+        {
+            preCalculatedTable[i] = 128;
+        }
+        if (i > a && i <= b)
+        {
+            preCalculatedTable[i] = ((255.0-128.0)/(b-a))*(i-a)+128;
+        }
+        if (i > b)
+        {
+            preCalculatedTable[i] = 255;
+        }
+    }
+    return preCalculatedTable;
+}
+
+void funcion(cv::Mat I, int a, int b)
+{
+    cv::Mat imgYUV;
+
+    cv::cvtColor(I, imgYUV, cv::COLOR_BGR2YCrCb);
+
+    float *t = getTransformTable(a, b);
+
+    for (int i=0;i<imgYUV.rows;i++)
+    {
+        for (int j=0;j<imgYUV.cols;j++)
+        {
+            //canal Y luminosidad
+            imgYUV.data[i*imgYUV.step + j*imgYUV.channels() + 0] = t[imgYUV.data[i*imgYUV.step + j*imgYUV.channels() + 0]];
+        }
+    }
+
+    cv::cvtColor(imgYUV, I, cv::COLOR_YCrCb2BGR);
+}
+
+int main(int argc, char *argv[])
+{
+
+    char* path = argv[1];
+
+    cv::Mat img;
+
+    img = cv::imread(path,1);
+
+    int a = atoi(argv[2]), b = atoi(argv[3]);
+
+    funcion(img, a, b);
+
+    cv::imshow("input", img);
+
+    cv::waitKey(0);
+
+    return 1;
+}
+
